0e143529dee1c993f3364d07338aa8e18b9795b3
SBammens <bammens1s@gmail.com>
2024-01-26 18:41:05 +0100

    Progress 26/01/2024

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Header from './components/Header';
+import Footer from './components/Footer'
+
+import Homepage from './pages/Homepage';
+import Servicespage from './pages/Servicespage';
+import Websitespage from './pages/Websitespage';
+import Automationpage from './pages/Automationpage';
+import CustomSolutionspage from './pages/CustomSolutionspage';
+
+import Contactpage from './pages/Contactpage';
+
+import styled from 'styled-components';
+
+const Content = styled.div`
+  flex: 1;
+`;
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      
+      <Content>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+
+          <Route exact path="/About" element={<Homepage />} />
+
+          <Route exact path="/Services" element={<Servicespage />} />
+          <Route exact path="/Services/Websites" element={<Websitespage />} />
+          <Route exact path="/Services/Automation" element={<Automationpage />} />
+          <Route exact path="/Services/Custom Solutions" element={<CustomSolutionspage />} />
+
+          <Route exact path="/Contact" element={<Contactpage />} />
+
+          <Route path="*" element={<Homepage />} />
+        </Routes>
+      </Content>
+      
+      <Footer/>
     </div>
+    </Router>
   );
 }
 
